83592844cfe0677e8bd463fba9d8173a77ac2118
GabrielOficina <gabrielarispe24@gmail.com>
2020-01-29 21:57:13 -0400

    Se empezo a trabajar en el algoritmo de los comentarios

--- src/Reporte.js
+++ src/Reporte.js
@@ -11,6 +11,10 @@ super();
 
 this.state={
 
+FechaCreacion:"",
+Nombre:"",
+Comentarios:[]
+
 
 
 }
@@ -39,27 +43,37 @@ if (this.props.ver===true) {
    
 <div className="fondoBarra rounded p-3">
 
-<h1 className="mr-3" align="right" Style="color:orange;">17-7-1999</h1>
+<h1 className="mr-3" align="right" Style="color:orange;">{this.state.FechaCreacion}</h1>
 
-<h1 className="display-4 text-primary mb-4" align="center">REPORTE {this.props.idbusqueda}</h1>
-
-
-
-<div className="bg-white mb-3  rounded pt-3 pl-3 pr-3 ">
+<h1 className="display-4 text-primary mb-4" align="center">{this.state.Nombre}</h1>
 
 
-<h1 className="h4">Los Tiburones de La Guaira son un equipo de béisbol venezolano perteneciente a la Liga Venezolana de Béisbol Profesional.</h1>
-<p className="font-weight-bold pb-2 mr-3" align="right">Gabriel Arispe  <span Style="color: #26926b ;">17-6-2006</span></p>
+{
+  //ALGORITMO PARA MOSTRAR TODOS LOS COMENTARIOS DEL REPORTE
+this.state.Comentarios.map((Elemento,indice)=>{
+return(
 
+<div className="bg-white mb-3  rounded pt-3 pl-3 pr-3" key={indice}>
+<h1 className="h4">{Elemento.comentario}</h1>
+<p className="font-weight-bold pb-2 mr-3" align="right">{Elemento.NombreUsuario}<span Style="color: #26926b ;">{Elemento.fechacomentario}</span></p>
 </div>
 
-<div className="fondoBarra p-3 rounded">
+)
 
+}
+
+
+)
+
+}
+
+
+
+<div className="fondoBarra p-3 rounded">
 
 <textarea name="message" rows="5" cols="90"  className="ml-3"></textarea>
 <button className="btn bg-white ml-3 mt-2" >Enviar</button>
 </div>
-
 
 </div>
 
