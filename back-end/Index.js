544c9e3cb79e7b3c761f5dcf74f5e13fc54c6373
GabrielArispe <gabrielarispe24@gmail.com>
2020-03-18 01:07:38 -0400

    Se trabajo en un algoritmo de prototipico para el manejo de las rutas

--- back-end/Index.js
+++ back-end/Index.js
@@ -24,6 +24,7 @@ VU.ValidameUsuario(req.body.Usuario,req.body.Contraseña,res);
 
 //CANTIDAD DE CLIENTES Y CANTIDAD DE CLIENTES CANCELADOS PARA CADA TIPO DE USUARIO
 
+
 //VIP
 app.get("/ClientesV",(req,res) => {
 
@@ -60,6 +61,58 @@ Clientes.CrearCliente(res,req.body.Nombre,req.body.Apellido,req.body.Correo,req.
 
 });
 
+
+/*
+//PROTOTIPO
+app.route("/Clientes:tipo").get((req,res) => {
+
+switch (req.params.tipo) {
+
+  case "VIP":
+
+Clientes.informeClientes(res,1);
+
+    break;
+
+    case "COMERCIAL":
+
+  Clientes.informeClientes(res,2);
+
+      break;
+
+      case "RESIDENCIAL":
+
+      Clientes.informeClientes(res,3);
+
+        break;
+
+  default:
+
+res.send("No buscas nada");
+
+break;
+
+}//FIN DEL SWITCH
+
+}).post((req,res) => {
+
+ if (req.params.tipo=="BUSCAR") {
+
+ Clientes.BuscaCliente(req.body.dato,req.body.Tipo,req.body.campo,res);
+
+ }
+
+else  if(req.params.tipo=="CREAR"){
+
+Clientes.CrearCliente(res,req.body.Nombre,req.body.Apellido,req.body.Correo,req.body.Direccion,req.body.Telefono,req.body.Tipo);
+
+}
+
+});
+
+
+
+*/
 
 
 
